0e2f51808df50f5837a1b4696b2e6e61064f1b95
William Tsay <willtsay@WTSAY.local>
2014-05-29 15:56:00 -0700

    helloworld

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -15,7 +15,7 @@ get '/google_login' do
                     "response_type=code&"+
                     "client_id=#{CLIENT_ID}&"+
                     "redirect_uri=http://hidden-reaches-5049.herokuapp.com/logged_in&"+
-                    "scope=profile&"+
+                    "scope=email%20profile&"+
                     "state=#{STATE}&" 
   redirect google_url
 end
@@ -24,6 +24,9 @@ get '/logged_in' do
   access_code = params[:code]
   first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token?", {body: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, redirect_uri: "http://hidden-reaches-5049.herokuapp.com/logged_in", grant_type:"authorization_code", code: access_code}})
   access_token = first_response["access_token"]
-  get_user_info = HTTParty.get("https://www.googleapis.com/plus/v1/people/me?", {query: {access_token: access_token}})
-  get_user_info.body
+  user_info = HTTParty.get("https://www.googleapis.com/plus/v1/people/me?", {query: {access_token: access_token}})
+  #check if their email is in the database, otherwise write them in. 
+  user_info.body
+  # if User.find_by_email(user_info["email"])
+  # user = User.create()
 end
