e00df2934624aed94669529061c61de1232f17b6
William Tsay <willtsay@WTSAY.local>
2014-05-29 14:22:39 -0700

    testing stuff sorry bout pushing to master- not

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,8 @@
 CLIENT_ID = "494745400143-ggt69ru9s24joh3ihik4psu91o74nlra.apps.googleusercontent.com"
 CLIENT_SECRET = "7-8xV3iGpxeFnio-mEX7Y6B4"
 STATE = SecureRandom.hex
+
+
 get '/' do
   erb :index
 end
@@ -19,6 +21,6 @@ get '/logged_in' do
   access_code = params[:code]
   first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token", {body: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, redirect: "http://hidden-reaches-5049.herokuapp.com/logged_in", grant_type:"authorization_code"}})
   access_token = first_response["access_token"]
-
   access_token
+  "hello"
 end
