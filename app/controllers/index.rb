853f41be1e04a6cf7b0d79511e9baba799cfce5e
William Tsay <willtsay@WTSAY.local>
2014-05-29 14:15:14 -0700

    fixed oauth to use codes/tokens

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
 CLIENT_ID = "494745400143-ggt69ru9s24joh3ihik4psu91o74nlra.apps.googleusercontent.com"
+CLIENT_SECRET = "7-8xV3iGpxeFnio-mEX7Y6B4"
 STATE = SecureRandom.hex
 get '/' do
   erb :index
@@ -6,7 +7,7 @@ end
 
 get '/google_login' do
   google_url = "https://accounts.google.com/o/oauth2/auth?" +
-                    "response_type=token&"+
+                    "response_type=code&"+
                     "client_id=#{CLIENT_ID}&"+
                     "redirect_uri=http://hidden-reaches-5049.herokuapp.com/logged_in&"+
                     "scope=profile&"+
@@ -15,5 +16,9 @@ get '/google_login' do
 end
 
 get '/logged_in' do
-  access_token = params[:access_token]
+  access_code = params[:code]
+  first_response = HTTParty.post("https://accounts.google.com/o/oauth2/token", {body: {client_id: CLIENT_ID, client_secret: CLIENT_SECRET, redirect: "http://hidden-reaches-5049.herokuapp.com/logged_in", grant_type:"authorization_code"}})
+  access_token = first_response["access_token"]
+
+  access_token
 end
